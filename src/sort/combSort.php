9cd9b62982b1d89dec2afe0f72a1419d105c742b
Vagrant Default User <vagrant@jessie.vagrantup.com>
2017-09-18 13:24:02 +0000

    adding comb sort

---
+++ src/sort/combSort.php
@@ -0,0 +1,28 @@
+<?php
+// コムソート
+function combSort(&$arr)
+{
+    $gap  = $size = count($arr);
+    $swap = true;
+    while($gap > 1 || $swap) {
+        if ($gap > 1 || $swap) {
+            $gap = floor($gap / 1.25);
+            if ($gap === 9 || $gap === 10) $gap = 11;
+        }
+        $swap = false;
+        $i = 0;
+        while ($i + $gap < $size) {
+            if ($arr[$i] > $arr[$i + $gap]) {
+                $tmp = $arr[$i];
+                $arr[$i] = $arr[$i + $gap];
+                $arr[$i + $gap] = $tmp;
+                $swap = true;
+            }
+            $i++;
+        }
+    }
+}
+
+$arr = [100, 3, 30, 20, 44, 32];
+combSort($arr);
+echo implode(", ", $arr). "\n";
